82fdbce57500438c5e9e228a406ca765d006cb86
HYDROTASMANIA\kunthurdodp <preetham.kunthurdoddihanumanthaiah@hydro.com.au>
2016-12-20 16:42:43 +1100

    external helper method

---
+++ HelperMethods/Infrastructure/CustomHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace HelperMethods.Infrastructure
+{
+    public static class CustomHelpers
+    {
+        public static MvcHtmlString ListArrayItems(this HtmlHelper html, string[] list)
+        {
+            TagBuilder tag = new TagBuilder("ul");
+
+            foreach (var str in list)
+            {
+                TagBuilder itemTag = new TagBuilder("li");
+                itemTag.SetInnerText(str);
+                tag.InnerHtml += itemTag.ToString();
+            }
+
+            return new MvcHtmlString(tag.ToString());
+        }
+    }
+}
