da9370130bb285ac1993f92908814e66d87bb7f5
HYDROTASMANIA\kunthurdodp <preetham.kunthurdoddihanumanthaiah@hydro.com.au>
2016-12-20 14:20:39 +1100

    initial commit helpermethods and branching

---
+++ HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Fruits = new String[] {"Apple", "Orange", "Pear"};
+            ViewBag.Cities = new String[] {"New York", "London", "Paris"};
+            string message = "This is an HTML element: <input>";
+            return View((object)message);
+        }
+    }
+}
