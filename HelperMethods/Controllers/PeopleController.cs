21fbd7ab0e36e45706bbd4dd80bdc55e8f80b65c
Preetham K H <preetham.kunthurdoddihanumanthaiah@hydro.com.au>
2016-12-29 23:11:53 +1100

    Ajax changes

---
+++ HelperMethods/Controllers/PeopleController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using HelperMethods.Models;
+
+namespace HelperMethods.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private Person[] personData = {
+            new Person {FirstName = "Adam", LastName = "Freeman", Role = Role.Admin},
+            new Person {FirstName = "Jacqui", LastName = "Griffyth", Role = Role.User},
+            new Person {FirstName = "John", LastName = "Smith", Role = Role.User},
+            new Person {FirstName = "Anne", LastName = "Jones", Role = Role.Guest}
+            };
+        
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult GetPeople(string selectedRole = "All")
+        {
+            //return View(personData);
+            return View((object) selectedRole);
+        }
+
+        //[HttpPost]
+        //public ActionResult GetPeople(string selectedRole)
+        //{
+        //    if (selectedRole == null || selectedRole == "All")
+        //    {
+        //        return View(personData);
+        //    }
+        //    var selected = (Role) Enum.Parse(typeof(Role), selectedRole);
+        //    return View(personData.Where(p => p.Role == selected));
+        //}
+
+        public PartialViewResult GetPeopleData(string selectedRole = "All")
+        {
+            IEnumerable<Person> data = personData;
+            if (selectedRole != "All")
+            {
+                var selected = (Role)Enum.Parse(typeof(Role), selectedRole);
+                data = personData.Where(p => p.Role == selected);
+            }
+
+            return PartialView(data);
+        }
+    }
+}
